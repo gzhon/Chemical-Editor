671efb3b5a03a56d7313651b06fa7a10085d41b8
Kei6327 <gzhongtian845@gmail.com>
2022-11-10 17:50:19 +0900

    Appクラス　コンストラクタ実装

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as PIXI from 'pixi.js'
+import { Application } from 'pixi.js';
 
 
 
@@ -7,5 +8,24 @@ import * as PIXI from 'pixi.js'
 
 
 window.onload = () =>{
+  //const app = new App()
+}
+class App {
 
+  private pixi!:PIXI.Application;
+
+  constructor(){
+
+    const app_id = document.getElementById('app')
+    if(app_id==null) return
+    const app_width = app_id.clientWidth;
+    this.pixi = new Application(
+      {
+        width:app_width,
+        height:530,
+        background:"white"
+      }
+    )
+    app_id.appendChild(this.pixi)
+  }
 }
