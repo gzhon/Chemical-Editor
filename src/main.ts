3dfd6ca50fd23547ff1a6574556e285ce790277a
gzhon <gzhongtian845@gmail.com>
2022-11-11 15:10:13 +0900

    ボタンへのクリックイベント実装

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
-import * as PIXI from 'pixi.js'
+import * as PIXI from 'pixi.js';
 import { Application } from 'pixi.js';
 
-
-
-
-
-
-
 window.onload = () =>{
-  //const app = new App()
+
+  const delete_btn = document.getElementById('delete');
+  const down_load_btn = document.getElementById('download');
+
+  if(delete_btn==null||down_load_btn==null) return
+
+  delete_btn.onclick = pixi_delete
+  down_load_btn.onclick = pixi_delete
+  const app = new App()
 }
 class App {
 
@@ -16,16 +18,23 @@ class App {
 
   constructor(){
 
-    const app_id = document.getElementById('app')
+    const app_id = document.getElementById('pixi')
     if(app_id==null) return
     const app_width = app_id.clientWidth;
     this.pixi = new Application(
       {
         width:app_width,
         height:530,
-        background:"white"
+        background:"red",
+        // @ts-ignore
+        view:app_id
       }
     )
-    app_id.appendChild(this.pixi)
   }
+}
+function pixi_delete(){
+
+}
+function svg_down_load(){
+
 }
