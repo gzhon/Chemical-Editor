9c67e6a7743eb077f832043a67f04f801d72201f
Kei6327 <gzhongtian845@gmail.com>
2022-11-16 13:31:37 +0900

    軽微な変更

--- src/DragText.ts
+++ src/DragText.ts
@@ -1,8 +1,11 @@
 import * as PIXI from 'pixi.js';
 
+const f= ""
+
 export class DragText extends PIXI.Text {
 
     constructor(txt:string){
-        super(txt,{fontSize:24})
+        super(txt,{fontSize:24,fill:"0xff1010"})
+        this.anchor.set(0.5)
     }
 }
